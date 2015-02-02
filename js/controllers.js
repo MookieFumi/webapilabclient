6645c56527e3063349e3dd802de70e91239e71cc
MookieFumi <mookiefumi@outlook.com>
2015-02-02 13:16:53 +0100

    Separate js files

---
+++ js/controllers.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function membersController($http) {
+    var url = "http://webapi21668.azurewebsites.net/api/";
+    var vm = this;
+    vm.members = [];
+    vm.towns = [];
+    vm.townToSearch = "";
+
+    vm.getMembers = function() {
+        $http.get(url + "members").
+        success(function(data, status, headers, config) {
+            getScope().vm.members = data;
+        }).
+        error(function(data, status, headers, config) {});
+    };
+
+    vm.searchTown = function() {
+        $http.get(url + "towns?name=" + vm.townToSearch).
+        success(function(data, status, headers, config) {
+            getScope().vm.towns = data;
+        }).
+        error(function(data, status, headers, config) {});
+    };
+
+    function getScope() {
+        var controllerElement = document.querySelector('[ng-controller="membersController as vm"]');
+        return angular.element(controllerElement).scope();
+    }
+}
+
+angular.module('webapilabclient').controller('membersController', membersController).$inject = ['$http'];
