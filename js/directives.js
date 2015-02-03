0732dfbef03a891fb476820570d6ae609bde97e7
MookieFumi <mookiefumi@outlook.com>
2015-02-03 13:53:04 +0100

    Add html template file cardTown/ cardMember

--- js/directives.js
+++ js/directives.js
@@ -4,7 +4,7 @@ angular.module('webapilabclient').directive('cardMember', function () {
         scope: {
             memberInfo: '=info'
         },
-        template: "<div class='col s12 m6'><div class='card blue-grey darken-1'><div class='card-content white-text'><span class='card-title'>{{memberInfo.Surname}}, {{memberInfo.Name}}</span><p>{{memberInfo.Departament}}&nbsp;</p></div></div></div>"
+        templateUrl: "templates/cardMember.html"
     };
 });
 
@@ -14,6 +14,6 @@ angular.module('webapilabclient').directive('cardTown', function () {
         scope: {
             townInfo: '=info'
         },
-        template: "<div class='col s12 m3'><div class='card blue darken-1'><div class='card-content white-text'><span class='card-title'>{{townInfo.Name}}</span><p>{{townInfo.Province}}&nbsp;</p></div></div></div>"
+        templateUrl: "templates/cardTown.html"
     };
 });
